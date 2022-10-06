6a7e3884e1eda90c902034cba1414eaebd603f21
Etec <gustavo.silva1667@etec.sp.gov.br>
2022-10-05 22:08:48 -0300

    add updates

---
+++ projects/deltaJS/resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('/css/normalize.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    </head>
+    <body class="page">
+        <header class="header-page">
+            <h1 class="logo logo-header">ΔJS</h1>
+        </header>
+    </body>
+</html>
