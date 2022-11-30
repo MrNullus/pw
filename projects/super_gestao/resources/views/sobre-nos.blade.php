56588405490efb6fb528770b9e21a145654e55cc
Etec <gustavo.silva1667@etec.sp.gov.br>
2022-11-30 19:23:54 -0300

    add imgs

--- projects/super_gestao/resources/views/sobre-nos.blade.php
+++ projects/super_gestao/resources/views/sobre-nos.blade.php
@@ -214,50 +214,33 @@
             </div>
         </div>
 
-        <div class="conteudo-destaque">
-        
-            <div class="esquerda">
-                <div class="informacoes">
-                    <h1>Sistema Super Gestão</h1>
-                    <p>Software para gestão empresarial ideal para sua empresa.<p>
-                    <div class="chamada">
-                        <img src="/img/check.png">
-                        <span class="texto-branco">Gestão completa e descomplicada</span>
-                    </div>
-                    <div class="chamada">
-                        <img src="img/check.png">
-                        <span class="texto-branco">Sua empresa na nuvem</span>
-                    </div>
-                </div>
-
-                <div class="video">
-                    <img src="img/player_video.jpg">
-                </div>
+        <div class="conteudo-pagina">
+            <div class="titulo-pagina">
+                <h1>Olá, eu sou o Super Gestão</h1>
             </div>
 
-            <div class="direita">
-                <div class="contato">
-                    <h1>Contato</h1>
-                    <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
-                    <form>
-                        <input type="text" placeholder="Nome" class="borda-branca">
-                        <br>
-                        <input type="text" placeholder="Telefone" class="borda-branca">
-                        <br>
-                        <input type="text" placeholder="E-mail" class="borda-branca">
-                        <br>
-                        <select class="borda-branca">
-                            <option value="">Qual o motivo do contato?</option>
-                            <option value="">Dúvida</option>
-                            <option value="">Elogio</option>
-                            <option value="">Reclamação</option>
-                        </select>
-                        <br>
-                        <textarea class="borda-branca">Preencha aqui a sua mensagem</textarea>
-                        <br>
-                        <button type="submit" class="borda-branca">ENVIAR</button>
-                    </form>
-                </div>
+            <div class="informacao-pagina">
+                <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
+                <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+            </div>  
+        </div>
+
+        <div class="rodape">
+            <div class="redes-sociais">
+                <h2>Redes sociais</h2>
+                <img src="img/facebook.png">
+                <img src="img/linkedin.png">
+                <img src="img/youtube.png">
+            </div>
+            <div class="area-contato">
+                <h2>Contato</h2>
+                <span>(11) 3333-4444</span>
+                <br>
+                <span>supergestao@dominio.com.br</span>
+            </div>
+            <div class="localizacao">
+                <h2>Localização</h2>
+                <img src="img/mapa.png">
             </div>
         </div>
     </body>
